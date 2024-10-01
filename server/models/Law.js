1b10cbb264681868ebb7ed1cfa6431fe11c72a25
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-09-30 21:16:23 -0400

    created law model and updated db name

---
+++ server/models/Law.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const commentSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        commentText: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        }
+    }
+);
+
+const lawSchema = new Schema(
+    {
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        location: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        source: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String, 
+            required: true,
+            trim: true,
+        },
+        comments: [commentSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+    }
+);
+
+const Law = model('Law', lawSchema);
+
+module.exports = Law;
